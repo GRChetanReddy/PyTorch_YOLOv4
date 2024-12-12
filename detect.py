140a45d8af83c2c0f3e1332bbaf1c541a95dee28
DipressedGuy <94971159+GRChetanReddy@users.noreply.github.com>
2024-12-12 16:59:06 +0530

    Update detect.py

--- detect.py
+++ detect.py
@@ -40,7 +40,7 @@ def detect(save_img=False):
     half = device.type != 'cpu'  # half precision only supported on CUDA
 
     # Load model
-    model = Darknet(cfg, imgsz)..to(device)
+    model = Darknet(cfg, imgsz).to(device)
     try:
         model.load_state_dict(torch.load(weights[0], map_location=device)['model'])
         #model = attempt_load(weights, map_location=device)  # load FP32 model
